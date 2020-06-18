f83fe61f82ed7e7a2a8102c71192aaf71fc07dcf
Jason S <jason.w.g.stewart@gmail.com>
2020-06-18 08:51:46 +0100

    added timer class

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+class Timer {
+  constructor(d, s, p) {
+    this.durationInput = d;
+    this.startButton = s;
+    this.pauseButton = p;
+    this.startButton.addEventListener("click", this.start);
+  }
+  start() {
+    console.log("Time to start!");
+  }
+  pause() {}
+  onDurationChange() {}
+  tick() {}
+}
+
+let d = document.querySelector("#duration");
+let s = document.querySelector("#start");
+let p = document.querySelector("#pause");
+
+let timer = new Timer(d, s, p);
